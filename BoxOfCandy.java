a351e7d9c00ebc329b0e0a90560957632b268c49
arikakira <samanthal242@nycstudents.net>
2025-04-10 00:56:39 +0000

    Update

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -13,15 +13,26 @@ public class BoxOfCandy
     * Returns false if there is no piece of candy in column col and returns true otherwise.
     * Precondition: col is a valid column index in box.
     */
-    public boolean moveCandyToFirstRow(int col)
-    { /* to be implemented in part (a) */ }
+//    public boolean moveCandyToFirstRow(int col)
+//    { /* to be implemented in part (a) */ }
 
     /**
     * Removes from box and returns a piece of candy with flavor specified by the parameter, or
     * returns null if no such piece is found, as described in part (b)
     */
-    public Candy removeNextByFlavor(String flavor)
-    { /* to be implemented in part (b) */ }
+ //   public Candy removeNextByFlavor(String flavor)
+ //   { /* to be implemented in part (b) */ }
 
     // There may be instance variables, constructors, and methods that are not shown.
+
+    public String toString() {
+        String s = "";
+        for(int row=0; row<box.length; row++) {
+            for(int col=0; col<box[0].length; col++) {
+                s += box[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    pubic static void main(String[] args) {
+    public static void main(String[] args) {
         Candy[][] box = new Candy[4][3];
         box[0][1] = new Candy("lime");
         box[1][1] = new Candy("orange");
